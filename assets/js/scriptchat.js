b147a8d269064987017dd8a8beba43c25a34dae1
Oscar Rodriguez <102395967+oskrmrg@users.noreply.github.com>
2023-05-09 17:09:06 -0400

    subir archivos

---
+++ assets/js/scriptchat.js
@@ -0,0 +1,24 @@
+// Metodos para mostrar la fecha y hora en cada uno de los chats
+const fechaChat1 = new Date();
+let fecha1 = fechaChat1.toLocaleString();
+document.getElementById("fecha1").innerHTML = fecha1;
+
+const fechaChat2 = new Date();
+let fecha2 = fechaChat2.toLocaleString();
+document.getElementById("fecha2").innerHTML = fecha2;
+
+const fechaChat3 = new Date();
+let fecha3 = fechaChat3.toLocaleString();
+document.getElementById("fecha3").innerHTML = fecha3;
+
+const fechaChat4 = new Date();
+let fecha4 = fechaChat4.toLocaleString();
+document.getElementById("fecha4").innerHTML = fecha4;
+
+const fechaChat5 = new Date();
+let fecha5 = fechaChat5.toLocaleString();
+document.getElementById("fecha5").innerHTML = fecha5;
+
+const fechaChat6 = new Date();
+let fecha6 = fechaChat6.toLocaleString();
+document.getElementById("fecha6").innerHTML = fecha6;
